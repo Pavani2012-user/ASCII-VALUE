f7a4b65515d1412f983d1655725d8387090540b4
Pavani2012-user <pavanibattu006@gmail.com>
2025-01-25 10:17:40 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,8 @@
+public class Ascii {
+        public static void main(String[] args) {
+            char ch='A';
+            System.out.println("Ascci value is "+(int)ch);
+        }
+    }
+    
+
